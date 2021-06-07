5536ccb8cfedb1a70b96deda49fda34db9d03a4d
rna-devknight <ricardo.nunesalcobia@knights.ucf.edu>
2021-06-06 20:47:05 -0400

    Reorganized function order for convenience

--- js/code.js
+++ js/code.js
@@ -91,7 +91,7 @@ function check() {
 }
 
 
-// Function to validate registration fields
+// Function to validate registration fields and prevent submission on failure
 function validate() {
 	// Obtains the two passwords
     var pw1 = document.getElementById("password").value;
@@ -103,6 +103,124 @@ function validate() {
 	else {
 		register();
 	}
+}
+
+// Function to register
+function register() {
+	// Obtain values from the form
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	var firstName = document.getElementById("firstName").value;
+	var lastName = document.getElementById("lastName").value;
+
+	// Initializes result
+    document.getElementById("result").innerHTML = "Processing, please wait...";
+
+	// Initializes the json payload and loads the url
+    var jsonPayload = '{"login" : "' + username + '", "password" : "' + password + '", "firstName" : "' + firstName + '", "lastName" : "' + lastName + '"}';
+    var url = address + registerAddress + extension;
+
+	// xhr = XMLHttpRequest
+	var xhr = new XMLHttpRequest();
+	// Initializes the newly created request
+	xhr.open("POST", url, true);
+	// Sets the value of the HTTP header (header, value)
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+	try {
+        xhr.onreadystatechange = function() {
+            // If request is finished and response is correct
+            if(this.readyState == 4 && this.status == 200) {
+                // Processes the JSON string and assigns server response
+                var jsonObject = JSON.parse(xhr.responseText);
+
+				// Displays confirmation message
+				document.getElementById("result").innerHTML = "Success!";
+
+                // Redirects to index.html
+                window.location.href = "index.html";
+            }
+        };
+
+        // Sends the request to the server
+        xhr.send(jsonPayload);
+    }
+    catch(err) {
+        // Throws error message in id result
+        document.getElementById("result").innerHTML = err.meessage;
+    }
+}
+
+// Function to search contacts
+function searchContacts() {
+    var search = document.getElementById("searchText").value;
+    document.getElementById("searchResult").innerHTML = "";
+
+    var contactList = "";
+
+    var jsonPayload = '{"search" : "' + search + '","userID" : ' + userID + '}';
+	var url = address + '/SearchContacts' + extension;
+
+    var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try {
+		xhr.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
+				var jsonObject = JSON.parse( xhr.responseText );
+				
+				for( var i=0; i<jsonObject.results.length; i++ ) {
+					contactList += jsonObject.results[i];
+					if( i < jsonObject.results.length - 1 )	{
+						contactList += "<br />\r\n";
+					}
+				}
+				
+				document.getElementsByTagName("p")[0].innerHTML = contactList;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err) {
+		document.getElementById("contactSearchResult").innerHTML = err.message;
+	}
+
+}
+
+// Function to add a contact
+function addContact() {
+    var newContact = document.getElementById("contactText").value;
+    document.getElementById("contactAddResult").innerHTML = "";
+
+    var jsonPayload = '{"contact" : "' + newContact + '", "userID" : ' + userID + '}';
+	var url = address + contactAddress + extension;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try	{
+		xhr.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				document.getElementById("addResult").innerHTML = "Contact has been added";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err) {
+		document.getElementById("addResult").innerHTML = err.message;
+	}
+}
+
+// Function to log the user out
+function logout() {
+    userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
 }
 
 // Function to save cookie from server info
@@ -151,122 +269,4 @@ function readCookie()
 		document.getElementById("welcome").innerHTML = message + firstName + " " + lastName + messageEnd;
 		document.getElementById("displayUsername").innerHTML = username;
 	}
-}
-
-// Function to log the user out
-function logout() {
-    userId = 0;
-	firstName = "";
-	lastName = "";
-	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-	window.location.href = "index.html";
-}
-
-// Function to add a contact
-function addContact() {
-    var newContact = document.getElementById("contactText").value;
-    document.getElementById("contactAddResult").innerHTML = "";
-
-    var jsonPayload = '{"contact" : "' + newContact + '", "userID" : ' + userID + '}';
-	var url = address + contactAddress + extension;
-
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-
-    try	{
-		xhr.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				document.getElementById("addResult").innerHTML = "Contact has been added";
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err) {
-		document.getElementById("addResult").innerHTML = err.message;
-	}
-}
-
-// Function to search contacts
-function searchContacts() {
-    var search = document.getElementById("searchText").value;
-    document.getElementById("searchResult").innerHTML = "";
-
-    var contactList = "";
-
-    var jsonPayload = '{"search" : "' + search + '","userID" : ' + userID + '}';
-	var url = address + '/SearchContacts' + extension;
-
-    var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-
-    try {
-		xhr.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
-				var jsonObject = JSON.parse( xhr.responseText );
-				
-				for( var i=0; i<jsonObject.results.length; i++ ) {
-					contactList += jsonObject.results[i];
-					if( i < jsonObject.results.length - 1 )	{
-						contactList += "<br />\r\n";
-					}
-				}
-				
-				document.getElementsByTagName("p")[0].innerHTML = contactList;
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err) {
-		document.getElementById("contactSearchResult").innerHTML = err.message;
-	}
-
-}
-
-// Function to register
-function register() {
-	// Obtain values from the form
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
-	var firstName = document.getElementById("firstName").value;
-	var lastName = document.getElementById("lastName").value;
-
-	// Initializes result
-    document.getElementById("result").innerHTML = "Processing, please wait...";
-
-	// Initializes the json payload and loads the url
-    var jsonPayload = '{"login" : "' + username + '", "password" : "' + password + '", "firstName" : "' + firstName + '", "lastName" : "' + lastName + '"}';
-    var url = address + registerAddress + extension;
-
-	// xhr = XMLHttpRequest
-	var xhr = new XMLHttpRequest();
-	// Initializes the newly created request
-	xhr.open("POST", url, true);
-	// Sets the value of the HTTP header (header, value)
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-
-	try {
-        xhr.onreadystatechange = function() {
-            // If request is finished and response is correct
-            if(this.readyState == 4 && this.status == 200) {
-                // Processes the JSON string and assigns server response
-                var jsonObject = JSON.parse(xhr.responseText);
-
-				// Displays confirmation message
-				document.getElementById("result").innerHTML = "Success!";
-
-                // Redirects to index.html
-                window.location.href = "index.html";
-            }
-        };
-
-        // Sends the request to the server
-        xhr.send(jsonPayload);
-    }
-    catch(err) {
-        // Throws error message in id result
-        document.getElementById("result").innerHTML = err.meessage;
-    }
 }
